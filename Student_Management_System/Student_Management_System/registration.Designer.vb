bcd6ead2337d978873223893a4c0d4358fc4a3de
RevathiRKNair <revathi.nair@in.bosch.com>
2023-01-29 18:54:11 +0530

    Registration and login update

---
+++ Student_Management_System/Student_Management_System/registration.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class registration
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.fname = New System.Windows.Forms.TextBox()
+        Me.lname = New System.Windows.Forms.TextBox()
+        Me.eid = New System.Windows.Forms.TextBox()
+        Me.dname = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.dphone = New System.Windows.Forms.TextBox()
+        Me.phone = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.address = New System.Windows.Forms.TextBox()
+        Me.gender = New System.Windows.Forms.ComboBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.course = New System.Windows.Forms.ComboBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.dept = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dob = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(433, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(164, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Registration"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(497, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Last Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(149, 200)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Email ID"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(149, 246)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 20)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "DOB"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(494, 246)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 20)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Gender"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(149, 297)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(102, 20)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "Father's Name"
+        '
+        'fname
+        '
+        Me.fname.Location = New System.Drawing.Point(268, 120)
+        Me.fname.Name = "fname"
+        Me.fname.Size = New System.Drawing.Size(157, 27)
+        Me.fname.TabIndex = 7
+        '
+        'lname
+        '
+        Me.lname.Location = New System.Drawing.Point(655, 120)
+        Me.lname.Name = "lname"
+        Me.lname.Size = New System.Drawing.Size(146, 27)
+        Me.lname.TabIndex = 8
+        '
+        'eid
+        '
+        Me.eid.Location = New System.Drawing.Point(262, 200)
+        Me.eid.Name = "eid"
+        Me.eid.Size = New System.Drawing.Size(163, 27)
+        Me.eid.TabIndex = 9
+        '
+        'dname
+        '
+        Me.dname.Location = New System.Drawing.Point(262, 297)
+        Me.dname.Name = "dname"
+        Me.dname.Size = New System.Drawing.Size(163, 27)
+        Me.dname.TabIndex = 12
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(490, 297)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(159, 20)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Father's phone number"
+        '
+        'dphone
+        '
+        Me.dphone.Location = New System.Drawing.Point(655, 297)
+        Me.dphone.Name = "dphone"
+        Me.dphone.Size = New System.Drawing.Size(146, 27)
+        Me.dphone.TabIndex = 18
+        '
+        'phone
+        '
+        Me.phone.Location = New System.Drawing.Point(655, 200)
+        Me.phone.Name = "phone"
+        Me.phone.Size = New System.Drawing.Size(146, 27)
+        Me.phone.TabIndex = 19
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(490, 200)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(105, 20)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Phone number"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(149, 352)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(62, 20)
+        Me.Label10.TabIndex = 21
+        Me.Label10.Text = "Address"
+        '
+        'address
+        '
+        Me.address.Location = New System.Drawing.Point(262, 352)
+        Me.address.Multiline = True
+        Me.address.Name = "address"
+        Me.address.Size = New System.Drawing.Size(539, 59)
+        Me.address.TabIndex = 22
+        '
+        'gender
+        '
+        Me.gender.FormattingEnabled = True
+        Me.gender.Items.AddRange(New Object() {"Male", "Female", "Others"})
+        Me.gender.Location = New System.Drawing.Point(655, 246)
+        Me.gender.Name = "gender"
+        Me.gender.Size = New System.Drawing.Size(146, 28)
+        Me.gender.TabIndex = 24
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(149, 447)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(89, 20)
+        Me.Label11.TabIndex = 26
+        Me.Label11.Text = "Department"
+        '
+        'course
+        '
+        Me.course.FormattingEnabled = True
+        Me.course.Location = New System.Drawing.Point(642, 444)
+        Me.course.Name = "course"
+        Me.course.Size = New System.Drawing.Size(159, 28)
+        Me.course.TabIndex = 28
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(497, 447)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(54, 20)
+        Me.Label12.TabIndex = 25
+        Me.Label12.Text = "Course"
+        '
+        'dept
+        '
+        Me.dept.FormattingEnabled = True
+        Me.dept.Items.AddRange(New Object() {"Commerce", "Life Science", "Computer Science", "English", "Psychology"})
+        Me.dept.Location = New System.Drawing.Point(247, 447)
+        Me.dept.Name = "dept"
+        Me.dept.Size = New System.Drawing.Size(151, 28)
+        Me.dept.TabIndex = 27
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(149, 495)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 41)
+        Me.Button1.TabIndex = 29
+        Me.Button1.Text = "REGISTER"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(457, 495)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(152, 35)
+        Me.Button2.TabIndex = 30
+        Me.Button2.Text = "RESET"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(775, 495)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(131, 35)
+        Me.Button3.TabIndex = 31
+        Me.Button3.Text = "CANCEL"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(149, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "First Name"
+        '
+        'dob
+        '
+        Me.dob.Location = New System.Drawing.Point(268, 243)
+        Me.dob.Name = "dob"
+        Me.dob.Size = New System.Drawing.Size(157, 27)
+        Me.dob.TabIndex = 32
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(145, 266)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(103, 20)
+        Me.Label13.TabIndex = 33
+        Me.Label13.Text = "(dd/mm/yyyy)"
+        '
+        'registration
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1024, 557)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.dob)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lname)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.fname)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.course)
+        Me.Controls.Add(Me.dept)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.gender)
+        Me.Controls.Add(Me.address)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.phone)
+        Me.Controls.Add(Me.dphone)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.dname)
+        Me.Controls.Add(Me.eid)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "registration"
+        Me.Text = "registration"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents fname As TextBox
+    Friend WithEvents lname As TextBox
+    Friend WithEvents eid As TextBox
+    Friend WithEvents dname As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents dphone As TextBox
+    Friend WithEvents phone As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents address As TextBox
+    Friend WithEvents gender As ComboBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents course As ComboBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents dept As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dob As TextBox
+    Friend WithEvents Label13 As Label
+End Class
---
+++ Student_Management_System/Student_Management_System/registration.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class registration
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim con As New SqlConnection
+            Dim cmd As New SqlCommand
+            Dim dr As SqlDataReader
+
+            con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\project\WinFormsApp7\WinFormsApp7\regi.mdf;Integrated Security=True"
+            con.Open()
+            cmd.Connection = con
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "select * from register where email='" & eid.Text & "' "
+            dr = cmd.ExecuteReader
+            If dr.HasRows Then
+                MsgBox("Email Id Already Registered", MsgBoxStyle.Critical)
+                con.Close()
+            Else
+                con.Close()
+                con.Open()
+                cmd = New SqlCommand("INSERT INTO register values('" & fname.Text & "','" & lname.Text & "','" & eid.Text & "','" & phone.Text & "','" & dob.Text & "','" & gender.Text & "','" & dname.Text & "','" & dphone.Text & "','" & address.Text & "','" & dept.Text & "','" & course.Text & "')", con)
+                If (fname.Text = "" And lname.Text = "" And eid.Text = "" And phone.Text = "" And dob.Text = "" And gender.Text = "" And dname.Text = "" And dphone.Text = "" And address.Text = "" And dept.Text = "" And course.Text = "") Then
+                    MessageBox.Show("Please enter the details")
+                Else
+                    cmd.ExecuteNonQuery()
+                    MsgBox("Succerssfully Registered.", MsgBoxStyle.Information, "Success")
+                    Me.Hide()
+
+                    fname.Clear()
+                    lname.Clear()
+                    eid.Clear()
+                    phone.Clear()
+                    dob.Clear()
+                    gender.Text = " "
+                    dname.Clear()
+                    dphone.Clear()
+                    address.Clear()
+                    dept.Text = " "
+                    course.Text = " "
+                End If
+                con.Close()
+            End If
+            con.Close()
+        Catch ex As Exception
+            MsgBox("Error")
+        End Try
+
+
+    End Sub
+
+    Private Sub course_SelectedIndexChanged(sender As Object, e As EventArgs) Handles course.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub registration_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
