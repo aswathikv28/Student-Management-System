a493ef99349fe8c6193cc60d075c4c8d9e91d78a
Aswathi K V <aswathikv0613@gmail.com>
2023-02-11 12:11:58 +0530

    Login update

--- Student_Management_System/Student_Management_System/login.Designer.vb
+++ Student_Management_System/Student_Management_System/login.Designer.vb
@@ -23,6 +23,11 @@ Partial Class login
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.email = New System.Windows.Forms.TextBox()
+        Me.password = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -34,11 +39,57 @@ Partial Class login
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "LOGIN"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(156, 161)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter Email ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(156, 275)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Enter Password"
+        '
+        'email
+        '
+        Me.email.Location = New System.Drawing.Point(339, 161)
+        Me.email.Name = "email"
+        Me.email.Size = New System.Drawing.Size(125, 27)
+        Me.email.TabIndex = 3
+        '
+        'password
+        '
+        Me.password.Location = New System.Drawing.Point(339, 275)
+        Me.password.Name = "password"
+        Me.password.Size = New System.Drawing.Size(125, 27)
+        Me.password.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(339, 364)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "LOGIN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.password)
+        Me.Controls.Add(Me.email)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "login"
         Me.Text = "login"
@@ -48,4 +99,9 @@ Partial Class login
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents email As TextBox
+    Friend WithEvents password As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- Student_Management_System/Student_Management_System/login.vb
+++ Student_Management_System/Student_Management_System/login.vb
@@ -1,3 +1,34 @@
-﻿Public Class login
+﻿Imports System.Data.SqlClient
 
+Public Class login
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\project\WinFormsApp7\WinFormsApp7\regi.mdf;Integrated Security=True"
+        Dim objcon As SqlConnection = Nothing
+    Dim objcmd As SqlCommand = Nothing
+
+    Try
+            objcon = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\project\WinFormsApp7\WinFormsApp7\regi.mdf;Integrated Security=True")
+            objcon.Open()
+Private Sub login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Dim stmt As String = "select * from register where email ='" & email.Text & "' AND password ='" & password.Text & "' "
+            objcmd = New SqlCommand(stmt, objcon)
+            Dim reader As SqlDataReader = objcmd.ExecuteReader
+    If reader.Read Then
+                MessageBox.Show("Login successfully")
+                Me.Hide()
+
+    Else
+                MessageBox.Show("Ivalid input")
+                form1.Show()
+                email.Clear()
+                password.Clear()
+
+            End If
+    Catch ex As Exception
+            MsgBox("MSSQL ERROR")
+        End Try
 End Class
